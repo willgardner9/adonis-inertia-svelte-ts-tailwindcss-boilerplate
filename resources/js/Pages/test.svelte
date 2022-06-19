505ff0f3b19f7029ab4dba2217926ffd95b71447
willgardner9 <willgardner9@gmail.com>
2022-06-19 09:44:07 +0100

    Initial commit

---
+++ resources/js/Pages/test.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let text: string
+</script>
+
+<h1>Hello <span>{text}!</span></h1>
+
+<style lang="scss">
+  h1 {
+    color: red;
+
+    span {
+      color: blue;
+    }
+  }
+</style>
